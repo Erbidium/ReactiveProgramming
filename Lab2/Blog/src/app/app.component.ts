0b010edbab79f55ca8b6e3f6859b917748e03955
Erbidium <OlexandrTanasienko@gmail.com>
2023-10-05 15:49:21 +0300

    Implement Blog app

--- Lab2/Blog/src/app/app.component.ts
+++ Lab2/Blog/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title:string;
+  text:string;
+  id?:number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Blog';
+  posts: Post[]=[
+    {title:'Вивчаю компоненти', text:'Створюю проект "Блог"', id:1},
+    {title:'Вивчаю директиви', text:'Все ще створюю "Блог"', id:2}
+  ];
+
+  updatePosts(post:Post){
+    this.posts.unshift(post);
+    console.log('Post',post);
+  }
+
+  deletePost(id:number){
+    this.posts = this.posts.filter(p=>p.id!==id)
+  }
 }
---
+++ Lab2/Blog/src/app/post/post.component.ts
@@ -0,0 +1,24 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import { Post } from '../app.component';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.sass']
+})
+export class PostComponent implements OnInit, OnDestroy {
+  @Input('toPost') myPost!:Post;
+  @Output() onRemove=new EventEmitter<number>();
+  constructor() { }
+
+  removePost(){
+    this.onRemove.emit(this.myPost.id)
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(){
+    console.log('метод ngOnDestroy');
+  }
+}
