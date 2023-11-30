649b0c33aeef3a79f2dc05fc35bd8315a075aba3
Erbidium <OlexandrTanasienko@gmail.com>
2023-11-30 18:01:53 +0200

    Implement app pipes 1

--- Lab6/Pipes1/src/app/app.component.ts
+++ Lab6/Pipes1/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+    <div>Без форматування: {{myDate}}</div>
+    <div>З форматуванням: {{myDate | date}}</div>
+    <div>{{welcome | uppercase}}</div>
+    <div>{{welcome | lowercase}}</div>
+    <div>{{persentage | percent}}</div>
+    <div>{{persentage | currency}}</div>
+    <div>{{welcome | slice:3}}</div>
+    <div>{{welcome | slice:6:11}}</div>
+    <div>{{myNewDate | date:"dd/MM/yyyy"}}</div>
+    <div>{{pi | number:'2.1-2'}}</div>
+    <div>{{pi | number:'3.5-5'}}</div>
+
+    <div>{{money | currency:'UA':'code'}}</div>
+    <div>{{money | currency:'UA':'symbol-narrow'}}</div>
+    <div>{{money | currency:'UA':'symbol':'1.1-1'}}</div>
+    <div>{{money | currency:'UA':'symbol-narrow':'1.1-1'}}</div>
+    <div>{{money | currency:'UA':'тільки сьогодні по ціні '}}</div>
+    <div>{{message | slice:6:11 | uppercase}}</div>
+
+    <div>Число до форматування: {{x}}<br>Число після форматування: {{x | format}}</div>
+
+    <hr/>
+
+    <div>{{users | join}}</div>
+    <div>{{users | join:1}}</div>
+    <div>{{users | join:1:3}}</div>
+    <div>{{num | sqrt}}</div>`
 })
 export class AppComponent {
-  title = 'Pipes1';
+  myDate = new Date(1961, 3, 12);
+  welcome: string = "Hello World!";
+  persentage: number = 0.14;
+  myNewDate = Date.now();
+  pi: number = 3.1415;
+  money: number = 23.45;
+  message = "Hello World!";
+  x: number = 15.45;
+  users = ["Tom", "Alice", "Sam", "Kate", "Bob"];
+  num: number = 9;
 }
--- Lab6/Pipes1/src/app/app.module.ts
+++ Lab6/Pipes1/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormatPipe} from "./format.pipe";
+import {JoinPipe} from "./join.pipe";
+import {SqrtPipe} from "./sqrt.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormatPipe,
+    JoinPipe,
+    SqrtPipe
   ],
   imports: [
     BrowserModule
